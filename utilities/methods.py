6f12ea1d86372b45a5a2b20dfc37904f36a00280
Douglas Oliveira <odoug@hotmail.com>
2022-03-03 20:50:06 -0300

    wip

--- utilities/methods.py
+++ utilities/methods.py
@@ -1,5 +1,8 @@
+from utilities.rl.deep_q_network import DeepQNetwork
 from utilities.io.stock_data_generator import StockDataGenerator
 from utilities.nn.neural_network import NeuralNetwork
+from utilities.rl.deep_q_network import DeepQNetwork
+
 import logging
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,6 +18,6 @@ def routine():
     nn = NeuralNetwork()
     
     model = nn.build(input_shape, output_action)
-    model.summary()
-
-    print("Here")
+    
+    dqn = DeepQNetwork()
+   
