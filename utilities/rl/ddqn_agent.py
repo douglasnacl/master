c2ca796cfda564addce71be640686ec8c9de9337
Douglas Oliveira <odoug@hotmail.com>
2022-03-11 01:29:45 -0300

    ref: ajustes em alguns comentários

--- utilities/rl/ddqn_agent.py
+++ utilities/rl/ddqn_agent.py
@@ -1,4 +1,3 @@
-from re import S
 from utilities.nn.neural_network import NeuralNetwork
 from utilities.utils.checks import track_results
 from collections import deque
@@ -10,32 +9,32 @@ import numpy as np
 
 
 class DDQNAgent:
-    def __init__(self, state_dim,
-                 num_actions,
-                 learning_rate,
-                 gamma,
-                 epsilon_start, 
-                 epsilon_end,
-                 epsilon_decay_steps,
-                 epsilon_exponential_decay,
-                 replay_capacity,
-                 architecture,
-                 l2_reg,
-                 tau,
-                 batch_size):
+    def __init__(self, state_dim,           # dimensões do estado
+                 num_actions,               # número de açoes possíveis
+                 learning_rate,             # taxa de aprendizado
+                 gamma,                     # fator de desconto
+                 epsilon_start,             # valor inicial de epsilon
+                 epsilon_end,               # valor final de epsilon
+                 epsilon_decay_steps,       # quantidade de passos de decaimento
+                 epsilon_exponential_decay, # decaimento exponencial para epsilon
+                 replay_capacity,           # capacidade de mecanismo de repetição (replay)
+                 architecture,              # arquitetura de rede neural utilizada em target_network e, por consequencia, online_network
+                 l2_reg,                    # taxa de regularização l2
+                 tau,                       # frequencia de atualização da rede neural
+                 batch_size):               # tamanho do lote
 
+        epsilon_start = 1.0
+        epsilon_end = .01
+        epsilon_decay_steps = 250
+        epsilon_exponential_decay = .99
 
-        # Número de dimensões do estado (trading_environment.observation_space.shape[0])
-        self.state_dim = state_dim
-        # Numero de Ações possíveis (trading_environment.action_space.n)
-        self.num_actions = num_actions
-        # Usamos deque para a janela necessária para a experiencia durante o replay
-        self.experience = deque([], maxlen=replay_capacity)
-        # Taxa de aprendizado alfa
-        self.learning_rate = learning_rate
-        # Fator de desconto 
+        self.state_dim = state_dim # num de dimensões do estado, dada por trading_environment.observation_space.shape[0]
+        self.num_actions = num_actions # num ações, dada por trading_environment.action_space.n
+        self.experience = deque([], maxlen=replay_capacity) # estabelece o tamanho do lote para a experiencia de replay do aprendizado
+
+        self.learning_rate = learning_rate 
         self.gamma = gamma
-        # Arquitetura da rede neural que será utilizada para 
+
         self.architecture = architecture
         self.l2_reg = l2_reg
 
@@ -46,20 +45,19 @@ class DDQNAgent:
 
         # A política $\epsilon$ começa com valor 'epsilon' e decai 'epsilon_decay' por 'epsilon_decay_steps'
         self.epsilon = epsilon_start
-        self.epsilon_decay_steps = epsilon_decay_steps
-        self.epsilon_decay = (epsilon_start - epsilon_end) / epsilon_decay_steps
-        self.epsilon_exponential_decay = epsilon_exponential_decay
+        self.epsilon_decay_steps = epsilon_decay_steps # *250
+        self.epsilon_decay = (epsilon_start - epsilon_end) / epsilon_decay_steps # (1 - 0.01) / 250 = 0.00396
+        self.epsilon_exponential_decay = epsilon_exponential_decay # 0.99
         self.epsilon_history = []
 
+        # Inicializa a função com valores nulos
         self.total_steps = self.train_steps = 0
         self.episodes = self.episode_length = self.train_episodes = 0
         self.steps_per_episode = []
         self.episode_reward = 0
         self.rewards_history = []
 
-        # Tamanho do batch para experience replay
         self.batch_size = batch_size
-        # Frequencia de ataulização da rede "target"
         self.tau = tau
         self.losses = []
         self.idx = tf.range(batch_size)
@@ -69,6 +67,7 @@ class DDQNAgent:
         
         n = len(self.architecture)
 
+        # cria uma instacia de network que será usada no aprendizado
         neural_network = NeuralNetwork(
             self.state_dim, 
             self.num_actions, 
@@ -77,28 +76,28 @@ class DDQNAgent:
             self.l2_reg)
 
         model = neural_network.build()
-
         return model
 
     def update_target(self):
-        # Atualiza os pesos de target_network usando os pesos de online_network
-        self.target_network.set_weights(self.online_network.get_weights())
+        self.target_network.set_weights(self.online_network.get_weights()) # atualiza target_network com os pesos da online_network
 
     def epsilon_greedy_policy(self, state):
-        # Incrementa o total de passos
-        self.total_steps += 1
-        # Exploration vs exploitation, se o numero aleatório for menor ou igual a epsilon
-        # Desta forma ele sempre iniciará explorando as ações
-        if np.random.rand() <= self.epsilon:
+        '''
+        Função responsavel pela implementação do paradigma Exporation vs Exploitation. 
+        Esta função escolhe entre uma ação aleatória ou aquela que maximia a recompensa (Qmax)
+        '''
+        self.total_steps += 1 # incrementa o contador de passos
+        if np.random.rand() <= self.epsilon: # se o numero aleatório for maior ou igual a epsilon, uma ação aleatório é executada
             return np.random.choice(self.num_actions)
         # Prevê o que usando a rede neural, retorna um conjunto de probilidade por ação
-        q = self.online_network.predict(state)
-        # pega a ação com maior probabilidade (max)
+        q = self.online_network.predict(state) # caso contrário, a ação tomada será aquela onde Q obtem seu máximo valor
         return np.argmax(q, axis=1).squeeze()
 
     def memorize_transition(self, state, action, reward, state_prime, not_done):
-        # Para a experiencia de repetição agente memoriza cada transição de estado 
-        # para que possa amostrar aleatoriamente um mini-lote durante o treinamento
+        '''
+        Para a experience replay o agente memoriza cada transição de estado, com isso em mãos será possível
+        amostrar aleatóriamente um mini-lote durante a fase de treinamento
+        '''
         if not_done:
             self.episode_reward += reward
             self.episode_length += 1
